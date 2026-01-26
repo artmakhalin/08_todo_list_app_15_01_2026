adba3190be4b4b8e165025386ac59fe95c252050
artmakhalin <artmakhalin@gmail.com>
2026-01-26 14:57:41 -0500

    createAsyncThunk added

--- src/reduxRTK/taskSlice.ts
+++ src/reduxRTK/taskSlice.ts
@@ -1,13 +1,42 @@
-import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
+import {
+  createAsyncThunk,
+  createSlice,
+  type PayloadAction,
+} from "@reduxjs/toolkit";
 import { loadFromLocalStorage, type ITask } from "../utils/constants";
 
 interface IState {
   tasks: ITask[];
+  loadingTask: boolean;
+  errorTask: string;
 }
 
 const initialState: IState = {
   tasks: loadFromLocalStorage<ITask>("tasks"),
+  loadingTask: false,
+  errorTask: "",
 };
+
+export const fetchTaskRTK = createAsyncThunk<ITask[]>(
+  "task/fetchTask",
+  async (_, thunkAPI) => {
+    try {
+      const res = await fetch(
+        "https://jsonplaceholder.typicode.com/todos?_limit=10",
+      );
+      if (!res.ok) {
+        throw new Error("Error when fetch tasks");
+      }
+      const data = (await res.json()) as Omit<ITask, "createdAt">[];
+      return data.map((t) => ({
+        ...t,
+        createdAt: new Date(2026, 0, 1, 0, 0, 0).toISOString(),
+      }));
+    } catch {
+      return thunkAPI.rejectWithValue("Network errorTask while fetching tasks");
+    }
+  },
+);
 
 const taskSlice = createSlice({
   name: "task",
@@ -24,13 +53,25 @@ const taskSlice = createSlice({
         e.id === action.payload.id ? action.payload : e,
       );
     },
-    fetchTask(state, action: PayloadAction<ITask[]>) {
-      state.tasks = action.payload;
-    },
+  },
+  extraReducers(builder) {
+    builder
+      .addCase(fetchTaskRTK.pending, (state) => {
+        state.loadingTask = true;
+        state.errorTask = "";
+      })
+      .addCase(fetchTaskRTK.fulfilled, (state, action) => {
+        state.loadingTask = false;
+        state.errorTask = "";
+        state.tasks = action.payload;
+      })
+      .addCase(fetchTaskRTK.rejected, (state, action) => {
+        state.loadingTask = false;
+        state.errorTask = (action.payload as string) || "Error";
+      });
   },
 });
 
-export const { createTask, deleteTask, editTask, fetchTask } =
-  taskSlice.actions;
+export const { createTask, deleteTask, editTask } = taskSlice.actions;
 
 export default taskSlice.reducer;
